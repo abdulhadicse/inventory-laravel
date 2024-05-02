cb6653cf20741fc8d11ee5517173f1882b9d46db
abdulhadi-selise <abdulhadi@selise.ch>
2024-05-02 23:33:39 +0600

    added logout

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -395,8 +395,15 @@
                     <a class="dropdown-item" href="#"><i class="ri-lock-unlock-line align-middle me-1"></i>
                         Lock screen</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item text-danger" href="#"><i
-                            class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
+
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <a class="dropdown-item text-danger" href="#"
+                            onclick="event.preventDefault();
+                    this.closest('form').submit();"><i
+                                class="ri-shut-down-line align-middle me-1 text-danger"></i>{{ __('Logout') }}</a>
+                    </form>
+
                 </div>
             </div>
         </div>
