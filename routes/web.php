f2b801d271288b8d602d4bd3e8d52c5123bbd165
abdulhadi-selise <abdulhadi@selise.ch>
2024-05-02 16:42:51 +0600

    added master blade files

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+	'/',
+	function () {
+		return view( 'welcome' );
+	}
+);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get(
+	'/dashboard',
+	function () {
+		return view( 'admin.layouts.master' );
+	}
+)->middleware( array( 'auth', 'verified' ) )->name( 'dashboard' );
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::middleware( 'auth' )->group(
+	function () {
+		Route::get( '/profile', array( ProfileController::class, 'edit' ) )->name( 'profile.edit' );
+		Route::patch( '/profile', array( ProfileController::class, 'update' ) )->name( 'profile.update' );
+		Route::delete( '/profile', array( ProfileController::class, 'destroy' ) )->name( 'profile.destroy' );
+	}
+);
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
